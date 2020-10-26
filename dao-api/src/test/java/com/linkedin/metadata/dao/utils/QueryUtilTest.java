8bde3fadaa1b28883835d0fd460b13647f7b9222
John Plaisted <jplaisted@linkedin.com>
2020-10-26 10:33:22 -0700

    Fix test after sync.

--- dao-api/src/test/java/com/linkedin/metadata/dao/utils/QueryUtilTest.java
+++ dao-api/src/test/java/com/linkedin/metadata/dao/utils/QueryUtilTest.java
@@ -1,9 +1,13 @@
 package com.linkedin.metadata.dao.utils;
 
+import com.google.common.collect.ImmutableSet;
+import com.linkedin.data.template.RecordTemplate;
 import com.linkedin.metadata.aspect.AspectVersion;
 import com.linkedin.metadata.query.Condition;
 import com.linkedin.metadata.query.Criterion;
 import com.linkedin.metadata.query.Filter;
+import com.linkedin.testing.AspectBar;
+import com.linkedin.testing.AspectFoo;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
@@ -25,9 +29,7 @@ public class QueryUtilTest {
 
   @Test
   public void testNewFilter() {
-    Map<String, String> params = Collections.singletonMap("foo", "bar");
-
-    Filter filter = QueryUtils.newFilter(params);
+    Filter filter = QueryUtils.newFilter("foo", "bar");
     assertEquals(filter.getCriteria().size(), 1);
     assertEquals(filter.getCriteria().get(0), QueryUtils.newCriterion("foo", "bar"));
 
@@ -35,6 +37,18 @@ public class QueryUtilTest {
     Map<String, String> paramsWithNulls = Collections.singletonMap("foo", null);
     filter = QueryUtils.newFilter(paramsWithNulls);
     assertEquals(filter.getCriteria().size(), 0);
+  }
+
+  @Test
+  public void testLatestAspectVersions() {
+    Set<Class<? extends RecordTemplate>> aspects = ImmutableSet.of(AspectFoo.class, AspectBar.class);
+
+    Set<AspectVersion> aspectVersions = QueryUtils.latestAspectVersions(aspects);
+
+    assertEquals(aspectVersions.size(), 2);
+
+    assertTrue(hasAspectVersion(aspectVersions, AspectFoo.class.getCanonicalName(), 0));
+    assertTrue(hasAspectVersion(aspectVersions, AspectBar.class.getCanonicalName(), 0));
   }
 
   private boolean hasAspectVersion(Set<AspectVersion> aspectVersions, String aspectName, long version) {
@@ -60,24 +74,24 @@ public class QueryUtilTest {
   }
 
   @Test
-  public void testIsHavingMore() {
+  public void testHasMore() {
 
     int totalCount = 23;
     int totalPageCount = QueryUtils.getTotalPageCount(totalCount, 10); // 3
 
-    boolean havingMore = QueryUtils.isHavingMore(0, 10, totalPageCount);
+    boolean havingMore = QueryUtils.hasMore(0, 10, totalPageCount);
     assertTrue(havingMore);
 
-    havingMore = QueryUtils.isHavingMore(10, 10, totalPageCount);
+    havingMore = QueryUtils.hasMore(10, 10, totalPageCount);
     assertTrue(havingMore);
 
-    havingMore = QueryUtils.isHavingMore(20, 10, totalPageCount);
+    havingMore = QueryUtils.hasMore(20, 10, totalPageCount);
     assertFalse(havingMore);
 
-    havingMore = QueryUtils.isHavingMore(30, 10, totalPageCount);
+    havingMore = QueryUtils.hasMore(30, 10, totalPageCount);
     assertFalse(havingMore);
 
-    havingMore = QueryUtils.isHavingMore(30, 0, totalPageCount);
+    havingMore = QueryUtils.hasMore(30, 0, totalPageCount);
     assertFalse(havingMore);
   }
 
