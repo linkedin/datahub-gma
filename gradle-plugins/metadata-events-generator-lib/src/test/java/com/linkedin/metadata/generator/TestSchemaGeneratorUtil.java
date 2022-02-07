855f76b8e00534fd0585be68937691b026fa116a
Tzu Ming Kuo <tkuo@tkuo-mn1.linkedin.biz>
2022-02-07 15:30:34 -0500

    feat(util): Added util functions in SchemaGeneratorUtil to get the namspace of a FQCN and determine if a GMA schema is a snaphot schema
    Added the following two methods to SchemaGeneratorUtil:
    (1) getNamespace, which returns the namespace of a given FQCN of a schema
    (2) isSnapshotSchema, which returns if a given schema is a GMA snapshot schema

--- gradle-plugins/metadata-events-generator-lib/src/test/java/com/linkedin/metadata/generator/TestSchemaGeneratorUtil.java
+++ gradle-plugins/metadata-events-generator-lib/src/test/java/com/linkedin/metadata/generator/TestSchemaGeneratorUtil.java
@@ -1,9 +1,12 @@
 package com.linkedin.metadata.generator;
 
+import com.linkedin.data.schema.Name;
+import com.linkedin.data.schema.RecordDataSchema;
 import org.junit.jupiter.api.Test;
 
 import static com.linkedin.metadata.generator.SchemaGeneratorUtil.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestSchemaGeneratorUtil {
@@ -28,5 +31,19 @@ public class TestSchemaGeneratorUtil {
   @Test
   public void testStripIllegalNamespace() {
     assertThatThrownBy(() -> stripNamespace(TEST_NAME + ".")).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  public void shouldGetNamespace() {
+    assertEquals(SchemaGeneratorUtil.getNamespace("com.a.b.A"), "com.a.b");
+  }
+
+  @Test
+  public void testIsSnapshotSchema() {
+    assertTrue(SchemaGeneratorUtil.isSnapshotSchema(new RecordDataSchema(new Name("XYZSnapshot"),
+        RecordDataSchema.RecordType.RECORD)));
+
+    assertFalse(SchemaGeneratorUtil.isSnapshotSchema(new RecordDataSchema(new Name("XYZ"),
+        RecordDataSchema.RecordType.RECORD)));
   }
 }
