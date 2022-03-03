066ae82a51e973c6173f929234d0da1c0064af13
Jesse Jia <jejia@linkedin.com>
2022-03-03 10:32:39 -0800

    Revert "fix: properly validate that aspects can only be of type record (#144)"
    This reverts commit d2d88bb47d769a86887269369684cf9d0f2900a3.

--- validators/src/main/java/com/linkedin/metadata/validator/ValidationUtils.java
+++ validators/src/main/java/com/linkedin/metadata/validator/ValidationUtils.java
@@ -157,9 +157,8 @@ public final class ValidationUtils {
         .collect(Collectors.toList());
   }
 
-  public static boolean isUnionWithOnlyRecordMembers(UnionDataSchema unionDataSchema) {
-    return unionDataSchema.getMembers().stream().allMatch(member -> member.getType().getDereferencedDataSchema().getType().equals(
-        DataSchema.Type.RECORD));
+  public static boolean isUnionWithOnlyComplexMembers(UnionDataSchema unionDataSchema) {
+    return unionDataSchema.getMembers().stream().allMatch(member -> member.getType().isComplex());
   }
 
   @Nonnull
