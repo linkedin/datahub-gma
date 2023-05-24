6b58ee4465b5f73c82f9255d1fc380a65d593bfc
Yang Yang <yanyang@linkedin.com>
2023-05-23 18:21:35 -0700

    (feat) generic aspect routing support (#254)
    * (feat) generic aspect routing support
    
    * pass setter method from the base aspect constructor

--- restli-resources/src/main/java/com/linkedin/metadata/restli/BaseGenericAspectRoutingGmsClient.java
+++ restli-resources/src/main/java/com/linkedin/metadata/restli/BaseGenericAspectRoutingGmsClient.java
@@ -5,28 +5,25 @@ import com.linkedin.data.template.RecordTemplate;
 import com.linkedin.restli.server.RestLiServiceException;
 import java.util.Set;
 
-
 /**
- * Deprecated, using {@link BaseGenericAspectRoutingGmsClient} instead.
  * <p>
  * A client interacts with standard GMS APIs.
  * </p>
  */
-@Deprecated
-public abstract class BaseAspectRoutingGmsClient<ASPECT extends RecordTemplate> {
+public abstract class BaseGenericAspectRoutingGmsClient {
 
   /**
    * Retrieves the latest version of the routing aspect for an entity.
    */
-  public abstract <URN extends Urn> ASPECT get(URN urn) throws RestLiServiceException;
+  public abstract <URN extends Urn> RecordTemplate get(URN urn, Class routingAspectClass) throws RestLiServiceException;
 
   /**
    * Backfill the routing aspect value for a given set of entity identified by the urns.
    */
-  public abstract <URN extends Urn> BackfillResult backfill(Set<URN> urn) throws RestLiServiceException;
+  public abstract <URN extends Urn> BackfillResult backfill(Set<URN> urn, Class routingAspectClass) throws RestLiServiceException;
 
   /**
    * Ingests the latest version of the routing aspect for an entity.
    */
-  public abstract <URN extends Urn> void ingest(URN urn, ASPECT aspect) throws RestLiServiceException;
+  public abstract <URN extends Urn> void ingest(URN urn, RecordTemplate aspect) throws RestLiServiceException;
 }
