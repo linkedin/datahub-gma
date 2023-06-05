337af626da34a7208111c4651673d13944f40cfc
Justin Donn <jdonn@linkedin.com>
2023-06-05 15:55:47 -0700

    feat(tracking): add createWithTracking method to BaseRemoteWriterDAO

--- dao-api/src/main/java/com/linkedin/metadata/dao/internal/BaseRemoteWriterDAO.java
+++ dao-api/src/main/java/com/linkedin/metadata/dao/internal/BaseRemoteWriterDAO.java
@@ -2,6 +2,7 @@ package com.linkedin.metadata.dao.internal;
 
 import com.linkedin.common.urn.Urn;
 import com.linkedin.data.template.RecordTemplate;
+import com.linkedin.metadata.events.IngestionTrackingContext;
 import javax.annotation.Nonnull;
 
 
@@ -21,4 +22,14 @@ public abstract class BaseRemoteWriterDAO {
    * @param <URN> must be the entity URN type in {@code SNAPSHOT}
    */
   abstract public <URN extends Urn> void create(@Nonnull URN urn, @Nonnull RecordTemplate snapshot);
+
+  /**
+   * Same as {@link #create(Urn, RecordTemplate)} but with tracking context attached.
+   * @param urn the {@link Urn} for the entity
+   * @param snapshot the snapshot containing updated metadata aspects
+   * @param trackingContext {@link IngestionTrackingContext} to use for DAO tracking probes and to pass on to the MAE
+   * @param <URN> must be the entity URN type in {@code SNAPSHOT}
+   */
+  abstract public <URN extends Urn> void createWithTracking(@Nonnull URN urn, @Nonnull RecordTemplate snapshot,
+      @Nonnull IngestionTrackingContext trackingContext);
 }
