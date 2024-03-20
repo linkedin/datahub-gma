03b818308b2c632c0bab0a78da010af017a98f86
Jesse Jia <jejia@jejia-mn2.linkedin.biz>
2024-03-19 17:04:25 -0700

    fix

--- gradle-plugins/metadata-annotations-lib/src/main/java/com/linkedin/metadata/annotations/AlwaysAllowList.java
+++ gradle-plugins/metadata-annotations-lib/src/main/java/com/linkedin/metadata/annotations/AlwaysAllowList.java
@@ -1,11 +1,9 @@
 package com.linkedin.metadata.annotations;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.linkedin.data.schema.DataSchema;
 import javax.annotation.Nonnull;
 
 
-@VisibleForTesting
 public class AlwaysAllowList implements GmaEntitiesAnnotationAllowList {
   @Override
   public void check(@Nonnull DataSchema schema, @Nonnull AspectAnnotation aspectAnnotation) {
