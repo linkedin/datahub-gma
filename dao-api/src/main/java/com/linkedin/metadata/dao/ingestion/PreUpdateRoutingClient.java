b32bd1c6713007c40794b756598210fbfdd388d2
Rakhi Agrawal <rakagrawal@linkedin.com>
2024-08-22 15:16:00 -0700

    Final interface decision

--- dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/PreUpdateRoutingClient.java
+++ dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/PreUpdateRoutingClient.java
@@ -1,21 +1,19 @@
 package com.linkedin.metadata.dao.ingestion;
 
 import com.google.protobuf.Message;
-import com.linkedin.common.urn.Urn;
 
 
 /**
  * An interface that defines methods to route update requests to the appropriate custom APIs.
  */
 
-public interface PreUpdateRoutingClient {
+public interface PreUpdateRoutingClient<ASSET extends Message> {
   /**
    * A method that routes the update request to the appropriate custom API.
    * @param urn the urn of the asset
-   * @param asset the asset to extract aspects from
-   * @param <ASSET> must be a valid asset model defined in com.linkedin.metadata.asset
-   * @return a routing result
+   * @param asset the aspect to update
+   * @return updated asset value
    * @throws Exception if the routing fails
    */
-  <ASSET extends Message> PreUpdateResult routingLambda(Urn urn, ASSET asset) throws Exception;
+  ASSET preUpdateLambda(Message urn, ASSET asset) throws Exception;
 }
