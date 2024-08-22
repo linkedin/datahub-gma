b8a2b3c8f24b25c136d3a9306e70aa755f2234ff
Rakhi Agrawal <rakagrawal@linkedin.com>
2024-08-21 20:59:17 -0700

    Added @Builder

--- dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/PreUpdateResult.java
+++ dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/PreUpdateResult.java
@@ -1,6 +1,7 @@
 package com.linkedin.metadata.dao.ingestion;
 
 import com.google.protobuf.Message;
+import lombok.Builder;
 import lombok.Data;
 
 
@@ -8,12 +9,8 @@ import lombok.Data;
  * A class that defines the result of the pre-update routing.
  */
 @Data
+@Builder
 public class PreUpdateResult<ASPECT extends Message> {
   private ExitRoutingStatus routingStatus;
   private ASPECT aspect;
-
-  public PreUpdateResult(ExitRoutingStatus routingStatus, ASPECT aspect) {
-    this.routingStatus = routingStatus;
-    this.aspect = aspect;
-  }
 }
