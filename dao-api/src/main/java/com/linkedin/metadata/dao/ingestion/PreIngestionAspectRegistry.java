cd63d96fb061549075b63dd57be9a0ccd989d320
Rakhi Agrawal <rakagrawal@linkedin.com>
2024-08-29 13:43:36 -0700

    Added javadoc

--- dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/PreIngestionAspectRegistry.java
+++ dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/PreIngestionAspectRegistry.java
@@ -3,7 +3,12 @@ package com.linkedin.metadata.dao.ingestion;
 import com.linkedin.data.template.RecordTemplate;
 import javax.annotation.Nonnull;
 
-
+/**
+ * A registry maintains which aspect needs PreIngestionRouting.
+ */
 public interface PreIngestionAspectRegistry {
+  /**
+   * Check if PreIngestionRouting is registered for an aspect.
+   */
   <ASPECT extends RecordTemplate> boolean isRegistered(@Nonnull final Class<ASPECT> aspectClass);
 }
--- dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/RestliCompliantPreUpdateRoutingClient.java
+++ dao-api/src/main/java/com/linkedin/metadata/dao/ingestion/RestliCompliantPreUpdateRoutingClient.java
@@ -7,9 +7,18 @@ import com.linkedin.data.template.RecordTemplate;
 
 
 public interface RestliCompliantPreUpdateRoutingClient<ASPECT extends Message> extends PreUpdateRoutingClient {
-  Message convertUrnToMessage(Urn pegasusUrn);
 
+  /**
+   * Converts a URN to a message.
+   */
+  Message convertUrnToMessage(Urn pegasusUrn);
+  /**
+   * Converts a record template aspect to message aspect.
+   */
   ASPECT convertAspectToMessage(RecordTemplate pegasusAspect);
 
+  /**
+   * Converts a message aspect to record template aspect.
+   */
   RecordTemplate convertAspectFromMessage(ASPECT messageAspect);
 }
