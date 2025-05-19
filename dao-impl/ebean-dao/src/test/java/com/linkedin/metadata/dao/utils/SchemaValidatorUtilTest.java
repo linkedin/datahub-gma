b04a0d4e4fff74c9c06cf22da6cc89018830fca4
Rakhi Agrawal <rakagrawal@linkedin.com>
2025-05-19 14:50:39 -0700

    Checkstyle

--- dao-impl/ebean-dao/src/test/java/com/linkedin/metadata/dao/utils/SchemaValidatorUtilTest.java
+++ dao-impl/ebean-dao/src/test/java/com/linkedin/metadata/dao/utils/SchemaValidatorUtilTest.java
@@ -1,15 +1,8 @@
 package com.linkedin.metadata.dao.utils;
 
 import com.google.common.io.Resources;
-import com.linkedin.common.AuditStamp;
 import com.linkedin.metadata.dao.EBeanDAOConfig;
-import com.linkedin.metadata.dao.EbeanLocalAccess;
 import com.linkedin.metadata.dao.EbeanLocalAccessTest;
-import com.linkedin.metadata.dao.IEbeanLocalAccess;
-import com.linkedin.metadata.dao.urnpath.EmptyPathExtractor;
-import com.linkedin.testing.AspectFoo;
-import com.linkedin.testing.urn.BurgerUrn;
-import com.linkedin.testing.urn.FooUrn;
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
 import java.io.IOException;
@@ -27,14 +20,13 @@ import static org.testng.AssertJUnit.*;
 
 public class SchemaValidatorUtilTest {
 
-  private static EbeanServer _server;
-  private static long _now;
-  private final EBeanDAOConfig _ebeanConfig = new EBeanDAOConfig();
-  private SchemaValidatorUtil _validator;
+  private static EbeanServer server;
+  private final EBeanDAOConfig ebeanConfig = new EBeanDAOConfig();
+  private SchemaValidatorUtil validator;
 
   @Factory(dataProvider = "inputList")
   public SchemaValidatorUtilTest(boolean nonDollarVirtualColumnsEnabled) {
-    _ebeanConfig.setNonDollarVirtualColumnsEnabled(nonDollarVirtualColumnsEnabled);
+    ebeanConfig.setNonDollarVirtualColumnsEnabled(nonDollarVirtualColumnsEnabled);
   }
 
   @DataProvider(name = "inputList")
@@ -47,41 +39,40 @@ public class SchemaValidatorUtilTest {
 
   @BeforeClass
   public void init() {
-    _server = EmbeddedMariaInstance.getServer(EbeanLocalAccessTest.class.getSimpleName());
-    _now = System.currentTimeMillis();
+    server = EmbeddedMariaInstance.getServer(SchemaValidatorUtilTest.class.getSimpleName());
   }
 
   @BeforeMethod
   public void setupTest() throws IOException {
-    if (!_ebeanConfig.isNonDollarVirtualColumnsEnabled()) {
-      _server.execute(Ebean.createSqlUpdate(
+    if (!ebeanConfig.isNonDollarVirtualColumnsEnabled()) {
+      server.execute(Ebean.createSqlUpdate(
           Resources.toString(Resources.getResource("ebean-local-access-create-all.sql"), StandardCharsets.UTF_8)));
     } else {
-      _server.execute(Ebean.createSqlUpdate(Resources.toString(
+      server.execute(Ebean.createSqlUpdate(Resources.toString(
           Resources.getResource("ebean-local-access-create-all-with-non-dollar-virtual-column-names.sql"),
           StandardCharsets.UTF_8)));
     }
-    _validator = new SchemaValidatorUtil(_server); // Instantiate validator
+    validator = new SchemaValidatorUtil(server); // Instantiate validator
   }
 
   @Test
   public void testCheckColumnExists() {
-    assertTrue(_validator.columnExists("metadata_entity_foo", "a_aspectfoo"));
-    assertFalse(_validator.columnExists("metadata_entity_foo", "a_aspect_not_exist"));
-    assertFalse(_validator.columnExists("metadata_entity_notexist", "a_aspectfoo"));
+    assertTrue(validator.columnExists("metadata_entity_foo", "a_aspectfoo"));
+    assertFalse(validator.columnExists("metadata_entity_foo", "a_aspect_not_exist"));
+    assertFalse(validator.columnExists("metadata_entity_notexist", "a_aspectfoo"));
 
-    if (!_ebeanConfig.isNonDollarVirtualColumnsEnabled()) {
-      assertTrue(_validator.columnExists("metadata_entity_foo", "i_aspectfoo$value"));
+    if (!ebeanConfig.isNonDollarVirtualColumnsEnabled()) {
+      assertTrue(validator.columnExists("metadata_entity_foo", "i_aspectfoo$value"));
     } else {
-      assertTrue(_validator.columnExists("metadata_entity_foo", "i_aspectfoo0value"));
+      assertTrue(validator.columnExists("metadata_entity_foo", "i_aspectfoo0value"));
     }
   }
 
   @Test
   public void testIndexExists() {
     // Change index name to match what exists in your schema
-    assertTrue(_validator.indexExists("metadata_entity_foo", "idx_aspect_bar"));
-    assertFalse(_validator.indexExists("metadata_entity_foo", "non_existent_index"));
+    assertTrue(validator.indexExists("metadata_entity_foo", "idx_aspect_bar"));
+    assertFalse(validator.indexExists("metadata_entity_foo", "non_existent_index"));
   }
 
 }
